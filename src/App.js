7668af9da4082c43f7fc0f51ec0944b73ff10020
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2023-10-12 10:15:40 +0300

    add style for responsive

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
-import AddTodoList from './components/todo/AddTodoList';
 import TodoList from './components/todo/TodoList';
-import ThemeProvider from 'react-bootstrap/ThemeProvider'
 function App() {
   return (
     <div className="App">
-      <ThemeProvider
-  breakpoints={[ 'sm', 'xs', 'xxs']}
-  minBreakpoint="xxs"
-> 
       <TodoList />
-      
-      </ThemeProvider>
-    </div>
+      </div>
   );
 }
 
